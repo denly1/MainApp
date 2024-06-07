9b48408dcf5e00a9f5cb845cde495cbf99edca46
Sasha <99606517+denly1@users.noreply.github.com>
2024-06-07 20:55:38 +0300

    Добавьте файлы проекта.

---
+++ MainApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace MainApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SerializeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var text = TextInput.Text;
+            var filePath = "text.json";
+
+            File.WriteAllText(filePath, text);
+
+            MessageBox.Show("Текст сохранен в файле text.json");
+        }
+
+        private void DeserializeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var filePath = openFileDialog.FileName;
+
+                var text = File.ReadAllText(filePath);
+
+                TextInput.Text = text;
+
+                MessageBox.Show("Текст успешно загружен из файла");
+            }
+        }
+    }
+}
---
+++ SerializationLibrary/Serializer .cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace SerializationLibrary
+{
+    public class Serializer
+    {
+        public static void Serialize<T>(T data, string filePath)
+        {
+            try
+            {
+                var options = new JsonSerializerOptions { WriteIndented = true };
+                var jsonData = JsonSerializer.Serialize(data, options);
+                File.WriteAllText(filePath, jsonData);
+            }
+            catch (Exception ex)
+            {
+                throw new SerializationException("Ошибка при сериализации данных", ex);
+            }
+        }
+
+        public static T Deserialize<T>(string filePath)
+        {
+            try
+            {
+                string jsonData;
+                using (var fileStream = File.OpenRead(filePath))
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    jsonData = streamReader.ReadToEnd();
+                }
+                return JsonSerializer.Deserialize<T>(jsonData);
+            }
+            catch (Exception ex)
+            {
+                throw new SerializationException("Ошибка при десериализации данных", ex);
+            }
+        }
+    }
+
+    public class SerializationException : Exception
+    {
+        public SerializationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
